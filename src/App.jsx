0e9c20d88242ef1cb6db807945f8526dda63ab0e
nischalpilmong <nischalrai04@gmail.com>
2025-01-23 18:31:02 +0100

    centered the text informarion and image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './App.css'
+
+
 import Home from './components/home/Home'
 import Services from './components/services/Services'
 import Skills from './components/skills/Skills'
